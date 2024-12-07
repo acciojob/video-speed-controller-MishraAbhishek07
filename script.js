04b54250f16647cb62eafda6ce34f6af73279635
MishraAbhishek07 <davmishra96@gmail.com>
2024-12-07 17:25:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-const inputs = document.querySelectorAll('.controls input');
+// script.js
+const video = document.querySelector('.viewer');
+const speed = document.querySelector('.speed');
+const speedBar = document.querySelector('.speed-bar');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+function handleSpeedChange(e) {
+    const y = e.pageY - speed.offsetTop;
+    const percent = y / speed.offsetHeight;
+    const min = 0.5;
+    const max = 2;
+    const height = Math.round(percent * 100) + '%';
+    const playbackRate = percent * (max - min) + min;
+    speedBar.style.height = height;
+    speedBar.textContent = playbackRate.toFixed(2) + '×';
+    video.playbackRate = playbackRate;
+}
+
+speed.addEventListener('mousemove', (e) => {
+    if (e.buttons === 1) {
+        handleSpeedChange(e);
     }
+});
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+speed.addEventListener('mousedown', handleSpeedChange);
